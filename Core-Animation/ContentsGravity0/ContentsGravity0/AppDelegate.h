9761f177a2675e20c2cf351d86350621e45e4764
ShannonMYang <myang.shannon1122@gmail.com>
2020-09-29 18:17:47 +0800

    新增Core Animation图层基础相关的demo

---
+++ Core-Animation/ContentsGravity0/ContentsGravity0/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ContentsGravity0
+//
+//  Created by 木溪鱼 on 2020/9/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Core-Animation/LayerTest0/LayerTest0/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LayerTest0
+//
+//  Created by 木溪鱼 on 2020/9/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
